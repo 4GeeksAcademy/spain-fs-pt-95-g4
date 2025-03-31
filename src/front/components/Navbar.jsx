d92ea9eba81b74de4c3de16037b90e4d10005362
alex15328 <alexmen15328@gmail.com>
2025-03-31 18:17:33 +0000

    cambios Navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,40 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Container, NavDropdown } from "react-bootstrap";
+import { Search } from "react-feather";
 
-export const Navbar = () => {
+const CustomNavbar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+      <Container>
+        <Navbar.Brand href="#home">TuLogo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Categorías" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#misticos">Lugares Místicos</NavDropdown.Item>
+              <NavDropdown.Item href="#paranormal">Paranormal</NavDropdown.Item>
+              <NavDropdown.Item href="#paises">Países</NavDropdown.Item>
+              <NavDropdown.Item href="#rutas">Rutas</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex search-form">
+  <FormControl
+    type="search"
+    placeholder="Buscar..."
+    className="me-2 search-input"
+    aria-label="Search"
+  />
+  <Search className="search-icon" />
+</Form>
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+          <Nav>
+            <Nav.Link href="#login">Iniciar sesión</Nav.Link>
+            <Nav.Link href="#register">Registrarse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
+
+export default CustomNavbar;
---
+++ src/front/imagesData.js
@@ -0,0 +1,15 @@
+
+//podemas ir agregando las imagenes aca para tener la galeria poco a poco//
+//esto es provisional//
+
+
+const imagesData = [
+  "https://picsum.photos/400/300?random=1",
+  "https://picsum.photos/400/300?random=2",
+  "https://picsum.photos/400/300?random=3",
+  "https://picsum.photos/400/300?random=4",
+  "https://picsum.photos/400/300?random=5",
+  "https://picsum.photos/400/300?random=6"
+];
+
+export default imagesData;

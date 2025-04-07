74ff2dbeb4a2cd14f5dae99387f75e03d9d2e3e2
alex15328 <alexmen15328@gmail.com>
2025-04-07 19:59:32 +0000

    cambios en agregar imagenes y boton de comentarios

--- src/front/imagesData.js
+++ src/front/imagesData.js
@@ -12,16 +12,51 @@ const imagesData = [
     description: "Esta iglesia está decorada con 30.000 huesos de víctimas de la peste, La iglesia gótica de Sedlec, cerca de la pintoresca ciudad de Kutná Hora,parece bastante normal desde fuera, pero si echas un vistazo al sótano descubrirás algo escalofriante.",
     comments: []
   },
-  {
+    {
     id: 2,
-    src: "/img/lugar2.jpg",
-    name: "Parque Natural",
-    description: "Naturaleza en estado puro",
+    src: "/img/imagen2.jpg",
+    name: "Bosque de Hoia Baciu",
+    description: "",
     comments: []
-  }
+  },
+  {
+    id: 3,
+    src: "/img/imagen3.jpg",
+    name: "Castillo de Bran, Rumanía",
+    description: "",
+    comments: []
+  },
+  {
+    id: 4,
+    src: "/img/imagen4.jpg",
+    name: "fvvhjghj",
+    description: "ghjhgjghjgh",
+    comments: []
+  },
+  {
+    id: 5,
+    src: "/img/imagen5.jpg",
+    name: "Isla de las Muñecas, México",
+    description: "Cientos de muñecas colgando de árboles.",
+    comments: []
+  },
+  {
+    id: 6,
+    src: "/img/imagen6.jpg",
+    name: "Catacumbas de París",
+    description: "Ossuarios con restos de más de 6 millones de personas.",
+    comments: []
+  },
+  {
+    id: 7,
+    src: "/img/imagen7.jpg",
+    name: "Pozo de Darvaza, Turkmenistán",
+    description: "Cráter de gas ardiendo desde 1971.",
+    comments: []
+  },
+  
 ];
 
 export default imagesData;
-
 
 
